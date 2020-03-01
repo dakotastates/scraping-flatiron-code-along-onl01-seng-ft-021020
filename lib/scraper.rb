d8ff24808bda8ec221d5bf7a8822ac19cd2abd61
Dakota States <dakota.states@gmail.com>
2020-03-01 07:23:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    binding.pry
+  
   end
   
   def print_courses
@@ -24,6 +24,6 @@ class Scraper
   
 end
 
-Scraper.new.get_page
+
 
 
